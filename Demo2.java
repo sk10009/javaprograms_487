bd882782dfc9ded7e4f6ff88001b972f1f37c777
sk10009 <salunkeshubham420@gmail.com>
2026-08-31 18:47:08 +0530

    31.8.26

---
+++ Demo2.java
@@ -0,0 +1,8 @@
+public class Main {
+    class demo{
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("Age: " + age);
+    }
+}
+}
---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Main {
+    class Student{
+    public static void main(String[] args) {
+        System.out.println("Welcome Student");
+    }
+} 
+}
